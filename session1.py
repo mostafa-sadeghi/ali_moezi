384212ae2b2767259d86e923195d6b226b2aafa9
msa <msai.sadehi@gmail.com>
2023-08-01 17:59:54 +0330

    first

---
+++ session1.py
@@ -0,0 +1,43 @@
+# number_one = 12
+# number_two = 12
+
+# print(number_one + number_two)
+# print(number_one - number_two)
+# print(number_one * number_two)
+# print(number_one / number_two)
+# print(number_one // number_two)
+# print(number_one % number_two)
+# print(number_one ** number_two)
+
+# x = 10
+# print(type(x))
+
+# x = 10.0
+# print(type(x))
+
+# x = 2 + 3 * 4
+# print(x)
+
+# x = (2 + 3) * 4
+# print(x)
+
+# message = "something"
+# print(type(message))
+# message = 'something'
+# print(type(message))
+# message = ''
+# print(type(message))
+# message = ""
+# print(type(message))
+
+# message = 'he said "something"'
+# print(message)
+# message = "he said \"something\""
+# print(message)
+
+
+message = "hello every body. \nwelcome to python class."
+print(message)
+message = """hello every body. 
+welcome to python class."""
+print(message)
